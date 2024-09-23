d204999a6f1bc8dbdeb65c0a0a3e5bf025c41091
Pejro-coder <peter.stepanic@hotmail.com>
2024-09-23 15:13:28 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,4 @@
-#TODO: Create a letter using starting_letter.txt 
-#for each name in invited_names.txt
-#Replace the [name] placeholder with the actual name.
-#Save the letters in the folder "ReadyToSend".
-    
-#Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
-    #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
-        #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
-
-
-with open("Input/Names/invited_names.txt", "r") as data_file:
+with open("invited_names.txt", "r") as data_file:
     names = data_file.readlines()
 
 new_names = []
@@ -16,11 +6,8 @@ for name in names:
     stripped_name = name.strip()
     new_names.append(stripped_name)
 
-with open("Input/Letters/starting_letter.txt") as data_file:
+with open("starting_letter.txt") as data_file:
     email = data_file.read()
-
-    baba = email.replace("Dear", "kala")
-    print(baba)
 
 for name in new_names:
     edited_email = email.replace("[name]", name)
